6ce27a7235f38b70bfb7a33a680b3e849532fe44
priashchy10 <priashchy10@gmail.com>
2023-02-21 10:25:33 +0600

    microservice commited

---
+++ JwtAuthenticationManager/Model/AuthenticationResponse.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JwtAuthenticationManager.Model
+{
+    public class AuthenticationResponse
+    {
+        public string Username { get; set; }
+        public string JwtToken { get; set; }
+        public int ExpiresIn { get; set; }
+    }
+}
